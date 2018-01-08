b2dd7480a69251fffd94c27980d02deaa84a44a1
brennanro <c00180359@itcarlow.ie>
2018-01-08 14:47:39 +0000

    Update studentTest.java

--- src/test/java/studentTest.java
+++ src/test/java/studentTest.java
@@ -32,6 +32,12 @@ public class studentTest {
         std.getAttendanceGrade(142);
     }
 
+      @org.junit.Test
+    public void absent() {
+        Student std = new Student();
+        assertEquals(Student.AttendanceGrade.ABSENT, std.getAttendanceGrade(0));
+    }
+
    
     @org.junit.Test
     public void lowVeryPoor() {

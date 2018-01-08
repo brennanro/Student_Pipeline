ff5765bebd814d9d117553f5fbd6efab708fce8f
brennanro <c00180359@itcarlow.ie>
2018-01-08 14:23:30 +0000

    Update updatedStudentClass.java

--- updatedStudentClass.java
+++ updatedStudentClass.java
@@ -1,4 +1,12 @@
+public class Student {
 
-public class updatedStudentClass {
+    public enum AttendanceGrade {VERY_POOR, AVERAGE, GOOD, VERY_GOOD};
 
+    public AttendanceGrade getAttendanceGrade(int attendance) {
+        if (attendance < 0 || attendance > 100) throw new IllegalArgumentException();
+        if (attendance < 30) return AttendanceGrade.VERY_POOR;
+        if (attendance < 70) return AttendanceGrade.AVERAGE;
+        if (attendance < 90) return AttendanceGrade.GOOD;
+        return AttendanceGrade.VERY_GOOD;
+    }
 }
